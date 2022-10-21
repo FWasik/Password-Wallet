f6327814cd3b52c41abf4e54c708694bdccb4a59
FWasik <80278631+FWasik@users.noreply.github.com>
2022-10-21 23:02:14 +0200

    user app commit with custom user, admin panel config and migration

---
+++ password_wallet/users/admin.py
@@ -0,0 +1,62 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
+from django import forms
+from django.contrib.auth.forms import ReadOnlyPasswordHashField
+
+
+class UserCreationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+
+    class Meta:
+        model = CustomUser
+        fields = ("login",)
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError("Passwords don't match")
+        return password2
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
+
+
+class UserChangeForm(forms.ModelForm):
+    password = ReadOnlyPasswordHashField()
+
+    class Meta:
+        model = CustomUser
+        fields = ("login", "password", "is_active", "is_staff", "is_superuser")
+
+    def clean_password(self):
+        return self.initial["password"]
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ("id", "login", "password", "last_login", "is_active", "is_staff", "is_superuser")
+    list_filter = ("is_superuser",)
+    fieldsets = (
+        (None, {'fields': ('login', 'password')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ("login", "password1", "password2"),
+        }),
+    )
+    search_fields = ('login',)
+    ordering = ('id',)
+
+
---
+++ password_wallet/users/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils import timezone
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, login: str, password: str, **extra_fields):
+        if not login:
+            raise ValueError(_("Login must be set"))
+
+        user = self.model(login=login, **extra_fields)
+        user.set_password(password)
+        user.save()
+
+        return user
+
+    def create_superuser(self, login: str, password: str, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+        extra_fields.setdefault("is_active", True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
+
+        return self.create_user(login, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    login = models.CharField(max_length=512, unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    USERNAME_FIELD = "login"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.login
+
+
+
+
