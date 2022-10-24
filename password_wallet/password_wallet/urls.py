44e006d9828ecbbc358b6b79b65a4c380e7c5614
FWasik <80278631+FWasik@users.noreply.github.com>
2022-10-24 12:58:12 +0200

    change password commit

--- password_wallet/password_wallet/urls.py
+++ password_wallet/password_wallet/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path("login/", auth_views.LoginView.as_view(template_name="users/login.html"), name="users-login"),
     path("register/",  views.register, name="users-register"),
     path("logout/", auth_views.LogoutView.as_view(template_name="users/logout.html"), name="users-logout"),
+    path("update-password/", views.change_password, name="users-password-change")
 ]
--- password_wallet/users/forms.py
+++ password_wallet/users/forms.py
@@ -7,3 +7,11 @@ class CustomUserCreationForm(UserCreationForm):
 	class Meta:
 		model = get_user_model()
 		fields = ("login", "password1", "password2")
+
+
+class CustomUserUpdateForm(forms.ModelForm):
+    password1 =  forms.CharField(max_length=250)
+
+    class Meta:
+        model = get_user_model()
+        fields = ["login", "password", "password1"]
